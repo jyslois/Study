e2a9372b52d616e01536ddb59e2a484356a12c0a
VisionPG <jyseolois@naver.com>
2023-01-04 18:27:37 +0900

    no message

---
+++ Algorithm/Jungol/input/Code9009.java
@@ -0,0 +1,8 @@
+public class Code9009 {
+    public static void main(String[] args) {
+        int a = 10;
+        String b = "A";
+
+        System.out.printf("%s%d%n%s%s", "a = ", a, "b = ", b);
+    }
+}
